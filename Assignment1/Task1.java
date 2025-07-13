28e3c8ce11f1c706df21d83638eace8ea0387d2e
Aayushzz <aayushnrla@gmail.com>
2025-07-13 17:20:10 -0400

    Completed Assignment 1

---
+++ Assignment1/Task1.java
@@ -0,0 +1,11 @@
+
+public class Task1 {
+    public static void main(String[] args) {
+        double length = 4.2;
+        double width = 2.4;
+
+        double area = length * width;
+
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
